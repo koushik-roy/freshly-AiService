7e712aa7854d2cbc6bba0ad3c76b396737cb7208
Koushik Roy <jc.koushikroy@gmail.com>
2025-12-06 22:35:16 +0530

    added cross origin

--- src/main/java/com/nekocodes/ai_service/controller/Controller.java
+++ src/main/java/com/nekocodes/ai_service/controller/Controller.java
@@ -3,6 +3,7 @@ package com.nekocodes.ai_service.controller;
 import com.nekocodes.ai_service.DTO.MealSuggestionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "*")
 public class Controller {
 
     private final AiService aiService;
